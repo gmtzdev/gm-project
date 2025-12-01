fc85755eef94d0facedfe987d1772d5422c54b34
gmtzdev <gerardomtzisc@gmail.com>
2025-12-01 09:04:54 -0600

    ⚡️ fix(api.auth): Use and adapt the new method and response by register controller

--- api/src/gmproject/auth/auth.controller.ts
+++ api/src/gmproject/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
@@ -9,14 +9,19 @@ import { Auth } from './decorators/auth.decorator';
 
 // Enums
 import { Role } from './enums/roles.enum';
+import { ApiTags } from '@nestjs/swagger';
+import { HttpResponse } from 'src/core/models/http/HttpResponse.model';
+import { RegisterResult } from './interfaces/registerResult.interface';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register') 
-  async register(@Body() registerDto: RegisterDto) {
-    return this.authService.register(registerDto);
+  async register(@Body() registerDto: RegisterDto): Promise<HttpResponse<RegisterResult>> {
+    const result = await this.authService.register(registerDto);
+    return new HttpResponse<RegisterResult>(true, 'User registered successfully', result, HttpStatus.CREATED);
   }
 
   @Post('login')
